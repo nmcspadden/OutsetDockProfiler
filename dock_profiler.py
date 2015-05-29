9d604696911e09576407e7590659e3089ec18bfc
Nick McSpadden <nmcspadden@gmail.com>
2015-05-29 09:33:43 -0700

    Cleaned up help information

--- dock_profiler.py
+++ dock_profiler.py
@@ -6,13 +6,13 @@ import shutil
 import tempfile
 import subprocess
 
-p = argparse.ArgumentParser()
-p.add_argument("-p","--profile", help='path to profile to load', required=True)
-p.add_argument("-n","--name", help='name of user to trigger on login', required=True)
-p.add_argument("-i","--identifier", help='identifier of package', default='com.organization.profile')
-p.add_argument("-o","--output", help='path to output package', default='Outset-Profile.pkg')
+p = argparse.ArgumentParser(description="Creates a package for Outset that will install a profile for a specific user on login.")
+p.add_argument("-p","--profile", help='path to profile to load. Required', required=True)
+p.add_argument("-n","--name", help='name of user to trigger on login. Required', required=True)
+p.add_argument("-i","--identifier", help='identifier of package, defaults to "com.organization.profile"', default='com.organization.profile')
+p.add_argument("-o","--output", help='path to output package, defaults to "Outset-Profile.pkg"', default='Outset-Profile.pkg')
 p.add_argument('--once', help='load profile once, not every login',action='store_true')
-p.add_argument("-s","--sign", help='sign package with valid identity')
+p.add_argument("-s","--sign", help='sign package with valid identity',metavar='IDENTITY')
 arguments = p.parse_args()
 
 libraryPath = 'Library/Profiles'
@@ -25,17 +25,14 @@ workingPath = tempfile.mkdtemp()
 os.makedirs(os.path.join(workingPath, libraryPath))
 shutil.copy(arguments.profile, os.path.join(workingPath, libraryPath))
 
-print "outsetPath: %s" % outsetPath
-print "WorkingPath: %s" % workingPath
-print "Combo: %s" % os.path.join(workingPath, outsetPath)
 # Place the script into the outset folder in the temporary path
 os.makedirs(os.path.join(workingPath, outsetPath))
-print "Temp path: %s" % os.path.join(workingPath, outsetPath)
 # This is the base profile installer script
 script='''#!/bin/sh
 if [[ $USER == "%s" ]]; then
     /usr/bin/profiles -IvF "/Library/Profiles/%s"
-fi''' % (arguments.name, os.path.basename(arguments.profile))
+fi
+''' % (arguments.name, os.path.basename(arguments.profile))
 # write script to file in temp directory
 with open(os.path.join(workingPath, outsetPath, 'profile-%s.sh' % arguments.name), 'wb') as outsetScript:
     outsetScript.write(script)
