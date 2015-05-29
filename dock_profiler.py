e7fa0b12515bb3ab7c7af6365709a78bfe5cbbc6
Nick McSpadden <nmcspadden@gmail.com>
2015-05-29 08:57:06 -0700

    Now allows signing

--- dock_profiler.py
+++ dock_profiler.py
@@ -12,31 +12,38 @@ p.add_argument("-n","--name", help='name of user to trigger on login', required=
 p.add_argument("-i","--identifier", help='identifier of package', default='com.organization.profile')
 p.add_argument("-o","--output", help='path to output package', default='Outset-Profile.pkg')
 p.add_argument('--once', help='load profile once, not every login',action='store_true')
+p.add_argument("-s","--sign", help='sign package with valid identity')
 arguments = p.parse_args()
 
 libraryPath = 'Library/Profiles'
 outsetPath = 'usr/local/outset/login-every'
 if (arguments.once):
-    outsetPath = '/usr/local/outset/login-once'
+    outsetPath = 'usr/local/outset/login-once'
 
 workingPath = tempfile.mkdtemp()
 # Copy the profile into the temporary path
-os.makedirs(os.path.join(workingPath, libraryPath)
+os.makedirs(os.path.join(workingPath, libraryPath))
 shutil.copy(arguments.profile, os.path.join(workingPath, libraryPath))
 
+print "outsetPath: %s" % outsetPath
+print "WorkingPath: %s" % workingPath
+print "Combo: %s" % os.path.join(workingPath, outsetPath)
 # Place the script into the outset folder in the temporary path
-os.makedirs(os.path.join(workingPath, libraryPath, outsetPath))
+os.makedirs(os.path.join(workingPath, outsetPath))
+print "Temp path: %s" % os.path.join(workingPath, outsetPath)
 # This is the base profile installer script
 script='''#!/bin/sh
 if [[ $USER == "%s" ]]; then
     /usr/bin/profiles -IvF "/Library/Profiles/%s"
 fi''' % (arguments.name, os.path.basename(arguments.profile))
 # write script to file in temp directory
-with open(os.path.join(workingPath, libraryPath, outsetPath, 'profile-%s.sh' % arguments.name), 'wb') as outsetScript:
+with open(os.path.join(workingPath, outsetPath, 'profile-%s.sh' % arguments.name), 'wb') as outsetScript:
     outsetScript.write(script)
-    
+os.chmod(os.path.join(workingPath, outsetPath, 'profile-%s.sh' % arguments.name), 0755)
 # Call productbuild to create a package out of the temp folder
 cmd = ['/usr/bin/productbuild', '--content', workingPath, '--identifier', arguments.identifier, arguments.output]
+if arguments.sign:
+    cmd += ['--sign', arguments.sign]
 proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 (pbout, pberr) = proc.communicate()
 
